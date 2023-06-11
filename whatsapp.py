c9063ab8e529f14ff8e982199c7b5e9deec5a573
shanto <shantoa729@gmail.com>
2023-06-11 17:04:14 +0900

    yess

---
+++ whatsapp.py
@@ -0,0 +1,8 @@
+import pywhatkit as p
+
+# phone_number = intput("Your phone number")
+p.sendwhatmsg("+8XXXxxxx", "XXXXxxxxxxxxxxXXXX", 17, 00, 15, True, 2)
+
+# For Group:
+group_id = intput("Group Id")
+p.sendwhatmsg_to_group("write id", "Test Group Mesg". 7, 31)
